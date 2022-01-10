335534fd5d6a3c3c6a9f5c6b9e11b94c606dc66c
jimpatel247 <pateljim2002@gmail.com>
2022-01-10 23:48:14 +0530

    React app created

---
+++ worldcouncelling/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="apna.jpeg"  className="App-logo" alt="logo" />
+        
+        <a
+          className="App-link"
+          href="public/apna.jpeg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          World councelling the best councelling company in the world
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
