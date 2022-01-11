d3cb44ae119eaa2b546a5b6c6fe2c683b63b5633
jimpatel247 <pateljim2002@gmail.com>
2022-01-11 15:00:10 +0530

    coursel added baki moj

--- worldcouncelling/src/Home.js
+++ worldcouncelling/src/Home.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Navbar from "./components/Navbar"
+import Coursel from './components/Coursel';
 
 function Home() {
   return (
     <>
         <Navbar/>
+        <Coursel/>
     </>
   );
 }
