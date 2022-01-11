9a23d457b3d65e3de7b6d80b6a1ceb80b35f55b0
07manan <07manan1850@gmail.com>
2022-01-11 15:53:10 +0530

    footer

--- worldcouncelling/src/Home.js
+++ worldcouncelling/src/Home.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Navbar from "./components/Navbar"
 import Coursel from './components/Coursel';
+import Footer from './components/Footer';
 
 function Home() {
   return (
     <>
         <Navbar/>
         <Coursel/>
+        <Footer/>
     </>
   );
 }
--- worldcouncelling/src/components/Footer.js
+++ worldcouncelling/src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import "../App.css"
 
 class Footer extends React.Component {
     render() { 
         return <>
-        Footer
+        <div className="footer row ">
+            <div className="contact col-lg-10">
+                <ul>
+                    <li><a target="_blank">Instagram</a></li>
+                    <li><a target="_blank" >Facebook</a></li>
+                    <li><a href='mailto:g.c.consultants02gmail.com' target="_blank">g.c.consultants02gmail.com</a></li>
+                    <li><a href='tel:+919795141300' target="_blank">+91 9795141300</a></li>
+                    <li><a href='tel:+917985806900' target="_blank">+91 7985806900</a></li>
+                </ul>
+            </div>
+            <div className="col-lg-2">
+                photo
+            </div>
+        </div>
         </>;
     }
 }
